c9726afc450c3e29965ba3b15fd02b8170f48312
Renata <renatateixeira.est@gmail.com>
2021-07-21 19:51:42 -0300

    Requisito 3 - Alem disso foi necessario acrescentar o space-between ao header e retirar as labels dos inputs

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload =function () {
+  const botao = document.querySelector("#submit");
+  botao.addEventListener("click", function(evnt){
+    const senha = document.querySelector("#password");
+    const login = document.querySelector("#email");
+    const valorSenha = senha.value;
+    const valorLogin = login.value;
+    const loginCorreto = "tryber@teste.com";
+    const senhaCorreta = "123456";
+
+    if ((valorLogin === loginCorreto) & (valorSenha === senhaCorreta)){
+      alert("Olá, Tryber!");
+    } else {
+      alert("Login ou senha inválidos.");
+    }
+  })
+}
